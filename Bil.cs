f25e9eeb0252db7b2baf710418cb35c6ebb1d544
Jesper Tim Damgaard Krøyer <jesp9030@edu.easj.dk>
2018-09-18 23:22:46 +0200

    Done Done

---
+++ Bil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using billetlibrary_Rovsneglen;
+namespace storebaeltlibrary_Rovsneglen
+{
+    public class Car: Bil
+    {
+
+        public Car()
+        {
+            
+        }
+
+        public double Pris(bool Brobizz, bool Weekend)
+        {
+            if (Brobizz)
+            {
+                if (Weekend)
+                {
+                    return 240 * 0.95 * 0.8;
+                }
+                return 240 * 0.95;
+            }
+            return 240;
+        }
+    }
+}
